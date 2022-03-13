d225cfd8c96180e0dbb743d3785e2f02205908ba
Alex <alexw213@gmail.com>
2022-03-13 19:22:26 +0100

    gui fix

--- main/main.py
+++ main/main.py
@@ -8,13 +8,26 @@ root.wm_title("Time-Control") # Fenster - Titel
 root.config(background = "#3399ff") # Hintergrundfarbe des Fensters
 root.geometry("1200x800")
 
-button1 = ttk.Button(root, text = "Coming")
+#Button 1
+def say_hello():
+    print("Have a nice workday!")
+
+button1 = ttk.Button(root, text = "Coming", padding=40, width=30, command=say_hello)
 button1.pack(side="left")
 
-button2 = ttk.Button(root, text = "Leaving")
+for item in button1.keys():
+    print(item, ": ", button1[item])
+
+#Button 2
+def say_bye():
+    print("Have a nice closing time!")
+
+button2 = ttk.Button(root, text = "Leaving", padding=40, width=30, command=say_bye)
 button2.pack(side="right")
 
-#button3 = ttk.Button(root, text = "Registration")
-#button3.pack(side="")
+#Button 3
+button3 = ttk.Button(root, text = "Registration", padding=20) # 3. Button mit Text "Registration" wird erstellt
+button3.pack(side="bottom", fill="x")   # 3. Button wird unten dargestellt und füllt den restlichen Freiraum der x-Achse
+                                        # aus
 
 root.mainloop()
