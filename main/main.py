093fdef16b604ec91f7445fcb913bd1ad73c9bf4
Alex <alexw213@gmail.com>
2022-03-15 14:44:15 +0100

    gui fix

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ root.config(background="#3399ff")  # Hintergrundfarbe des Fensters
 root.geometry("1200x800")  # GUI-Fenstergröße bestimmen
 
 
-#%% Buttons
+#%% --- GUI ---
 
 # Button 1
 def say_hello():
@@ -33,10 +33,9 @@ button2.pack(side="right", padx=20, pady=50)
 
 
 
-#%% Registration-Entry
+#%% Registration on a new window
 def createNewWindow():
     popup = tk.Toplevel(root)
-
 
 fields = 'Last Name', 'First Name', 'Date of Birth', 'Marital status' , 'Adress' , 'Phone number' , 'E-Mail' , 'RFID-Tag'
 
@@ -59,56 +58,20 @@ def makeform(root, fields):
    return entries
 
 if __name__ == '__main__':
-   popup = tk.Toplevel(root)
-   ents = makeform(popup, fields)
-   popup.bind('<Return>', (lambda event, e=ents: fetch(e)))
-   b1 = Button(popup, text='Show',
+    popup = tk.Toplevel(root)
+    ents = makeform(popup, fields)
+    popup.bind('<Return>', (lambda event, e=ents: fetch(e)))
+    b1 = Button(popup, text='Show',
           command=(lambda e=ents: fetch(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
-   b2 = Button(popup, text='Quit', command=popup.quit)
-   b2.pack(side=LEFT, padx=5, pady=5)
+    b1.pack(side=LEFT, padx=5, pady=5)
+    b2 = Button(popup, text='Quit', command=popup.quit)
+    b2.pack(side=LEFT, padx=5, pady=5)
 
-
-button3 = tk.Button(root,
+    b3 = tk.Button(root,
                 text="Registration",
                 padx=20, pady=10,
                 command=createNewWindow)
-button3.pack(side="bottom", fill="x", pady=20)
+    b3.pack(side="bottom", fill="x", pady=20)
 
-
-
-"""
-# Button 3
-def createNewWindow():
-    newWindow = tk.Toplevel(app)  # neues Fenster in root
-
-button3 = ttk.Button(root, text="Registration", command=createNewWindow)
-button3.pack(side="bottom", fill="x")
-
-
-# Eingabefeld
-def print_entry_input():
-    print(entry1_popup.get())
-
-
-entry1_popup = ttk.Entry(master=popup)
-entry1_popup.pack()
-"""
-"""
-button4 = ttk.Button(root, text = "Confirm", command=print_entry_input) #Gibt eingebene Daten aus
-button4.pack(side="bottom", fill="x")
-
-def delete_input():
-    entry1.delete(0, tk.END)
-
-button5 = ttk.Button(root, text = "Clear", command=delete_input) #Löscht eingegebene Daten
-button5.pack(side="bottom", fill="x")
-
-#for item in entry1.keys():
-#    print(item, ": ", entry1[item])
-
-for item in button5.keys():
-    print(item, ": ", button5[item])
-"""
 
 root.mainloop()
