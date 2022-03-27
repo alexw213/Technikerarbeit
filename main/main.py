e83f78f3e4849d70321da07fb231c75e9e778c78
Alex <alexw213@gmail.com>
2022-03-27 21:37:11 +0200

    try

--- main/main.py
+++ main/main.py
@@ -140,7 +140,7 @@ def register():
 
         camera.take_picture()
 
-        canvas = Canvas(popup, width=300, height=300)
+        canvas = Canvas(root, width=300, height=300)
         canvas.pack()
         img = ImageTk.PhotoImage(Image.open("/home/pi/Technikerarbeit/camera/Pictures/image.jpg"))
         canvas.create_image(20, 20, anchor=NW, image=img)
