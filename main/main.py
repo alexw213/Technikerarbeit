21163ccb54ea9b25b69249cd7cb3bb6523540b66
Alex <alexw213@gmail.com>
2022-03-27 21:58:09 +0200

    try

--- main/main.py
+++ main/main.py
@@ -1,3 +1,4 @@
+"""
 import tkinter as tk  # tkinter abkürzen mit tk
 from tkinter import *  # Importierung der ttk-Widgets
 from tkinter import messagebox
@@ -181,4 +182,30 @@ protokoll.pack(side='top', fill='x', padx=20, pady=30)
 
 
 root.mainloop()
+"""
+import tkinter
+import cv2
+import PIL.Image, PIL.ImageTk
 
+# Create a window
+window = tkinter.Tk()
+window.title("OpenCV and Tkinter")
+
+# Load an image using OpenCV
+cv_img = cv2.cvtColor(cv2.imread("/home/pi/Technikerarbeit/camera/Pictures/image.jpg"), cv2.COLOR_BGR2RGB)
+
+# Get the image dimensions (OpenCV stores image data as NumPy ndarray)
+height, width, no_channels = cv_img.shape
+
+# Create a canvas that can fit the above image
+canvas = tkinter.Canvas(window, width = width, height = height)
+canvas.pack()
+
+# Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+
+# Add a PhotoImage to the Canvas
+canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+
+# Run the window loop
+window.mainloop()
