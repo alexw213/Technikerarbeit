498820773b5c055feace986caf59619c583a19cb
Alex <alexw213@gmail.com>
2022-03-31 17:49:40 +0200

    camera fix

--- main/main.py
+++ main/main.py
@@ -7,6 +7,8 @@ from db import write_db
 from db import read_db
 from camera import camera
 from PIL import ImageTk,Image
+import cv2
+
 
 root = tk.Tk()  # Fenster erstellen
 root.wm_title('Time-Control')  # Fenster - Titel
@@ -140,9 +142,21 @@ def register():
 
         camera.take_picture()
 
-        img = ImageTk.PhotoImage(Image.open("/home/pi/Technikerarbeit/camera/Pictures/image.jpg"))
-        panel = tk.Label(root, image=img)
-        panel.pack(anchor=NW)
+        # Load an image using OpenCV
+        cv_img = cv2.cvtColor(cv2.imread("/home/pi/Technikerarbeit/camera/Pictures/image.jpg"), cv2.COLOR_BGR2RGB)
+
+        # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
+        height, width, no_channels = cv_img.shape
+
+        # Create a canvas that can fit the above image
+        canvas = tk.Canvas(root, width=width, height=height)
+        canvas.pack()
+
+        # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+        photo = PhotoImage(image=Image.fromarray(cv_img))
+
+        # Add a PhotoImage to the Canvas
+        canvas.create_image(0, 0, image=photo, anchor=tk.NW)
 
     b2 = tk.Button(popup,
                    text='Foto',
@@ -181,31 +195,3 @@ protokoll.pack(side='top', fill='x', padx=20, pady=30)
 
 
 root.mainloop()
-
-import tkinter
-import cv2
-import PIL.Image, PIL.ImageTk
-"""
-# Create a window
-window = tkinter.Tk()
-window.title("OpenCV and Tkinter")
-
-# Load an image using OpenCV
-cv_img = cv2.cvtColor(cv2.imread("/home/pi/Technikerarbeit/camera/Pictures/image.jpg"), cv2.COLOR_BGR2RGB)
-
-# Get the image dimensions (OpenCV stores image data as NumPy ndarray)
-height, width, no_channels = cv_img.shape
-
-# Create a canvas that can fit the above image
-canvas = tkinter.Canvas(window, width = width, height = height)
-canvas.pack()
-
-# Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
-photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
-
-# Add a PhotoImage to the Canvas
-canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
-
-# Run the window loop
-window.mainloop()
-"""
