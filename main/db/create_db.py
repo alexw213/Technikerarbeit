8dc4d519db7210748b82925c803e7ff2c3bf3545
Alex <alexw213gmail.com>
2022-04-16 12:34:53 +0200

    asd

--- main/db/create_db.py
+++ main/db/create_db.py
@@ -3,14 +3,14 @@ import sqlite3
 connection = sqlite3.connect('data.db') # Verbindung zur Datenbank "data.db" wird aufgebaut
 cursor = connection.cursor() # Objekt zur SQL Navigation wird erstellt
 
-# User Tabelle wird erstellt
+# User Tabelle wird erstellt!
 query = "CREATE TABLE IF NOT EXISTS user (vorname text, " \
                "nachname text, geburtsdatum date, familienstand text, adresse text, telefonnummer int, " \
                "email text, rfidtag int, pid int, PRIMARY KEY (pid))"
 cursor.execute(query) # Exexute-Methode wird aufgerufen und SQL Befehl "create_table" wird ausgeführt
 connection.commit() # Vorherige Ausführungen werden bestätigt
 
-# Protokolltabelle wird erstellt
+# Protokolltabelle wird erstellt!
 query = "CREATE TABLE IF NOT EXISTS protocol (rfidtag INT, zeitpunkt TIMESTAMP, reg_art TEXT)"
 cursor.execute(query)
 connection.commit()
